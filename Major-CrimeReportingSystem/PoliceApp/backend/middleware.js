252d58b7bf1097e2b1a622f69b48300390879988
Priyanshu <pradipsinghb601@gmail.com>
2025-08-31 00:56:50 +0530

    ho gya

--- Major-CrimeReportingSystem/PoliceApp/backend/middleware.js
+++
@@ -1,7 +0,0 @@
-module.exports.PoliceAuth = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  } else {
-    return res.status(401).json({ success: false, message: "Unauthorized" });
-  }
-};
